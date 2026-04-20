d90e706f1478209e4122b2ccb6b363755611b83f
ZeynebKoc <koczeyneb@gmail.com>
2026-04-21 00:52:50 +0300

    feat: add core Todo types

---
+++ src/types/todo.types.ts
@@ -0,0 +1,22 @@
+export enum TaskStatus {
+  TODO = "todo",
+  IN_PROGRESS = "in_progress",
+  DONE = "done",
+}
+
+export enum TaskPriority {
+  LOW = "low",
+  MEDIUM = "medium",
+  HIGH = "high",
+}
+
+export interface Todo {
+  id: string;
+  title: string;
+  description: string;
+  status: TaskStatus;
+  priority: TaskPriority;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+}
